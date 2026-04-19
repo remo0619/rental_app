dc363258820cf62cc5d9e48fa80ed078166d604a
remo0619 <ichinohe@aomori-innovators.com>
2026-04-19 20:13:38 +0900

    機器登録機能実装

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,14 +1,25 @@
 class DevicesController < ApplicationController
+  before_action :set_device, only: [:show, :edit, :update, :destroy]
+
   def index
+    @devices = Device.all
   end
 
   def show
+    p @devices
   end
 
   def new
+    @device = Device.new
   end
 
   def create
+    @device = current_user.devices.build(set_params)
+    if @device.save
+      redirect_to devices_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,5 +29,15 @@ class DevicesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_device
+    @device = Device.find(params[:id])
+  end
+
+  def set_params
+    params.require(:device).permit(:device_type, :serial)
   end
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -3,4 +3,8 @@ class Device < ApplicationRecord
   has_many :requests
 
   enum status: { available: 0, unavailable: 1 }
+
+  validates :serial, length: { maximum: 30 }, presence: true,
+                      uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :device_type, length: { maximum: 20 }, presence: true
 end

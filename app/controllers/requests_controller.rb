feb625aa3668d0b35457aadcfa85a58b50190cd5
remo0619 <ichinohe@aomori-innovators.com>
2026-04-20 14:54:52 +0900

    申請フォーム追加

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,6 @@
 class RequestsController < ApplicationController
+  before_action :set_request, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -6,9 +8,17 @@ class RequestsController < ApplicationController
   end
 
   def new
+    @request = Request.new
+    @device = Device.find(params[:device_id])
   end
 
   def create
+    @request = current_user.requests.build(set_params)
+    if @request.save
+      redirect_to requests_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,5 +28,15 @@ class RequestsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_request
+    @request = Request.find(params[:id])
+  end
+
+  def set_params
+    params.require(:request).permit(:start_date, :end_date)
   end
 end
